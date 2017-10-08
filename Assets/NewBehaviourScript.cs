83c8654b745ad92d3f6bad1ee1b885e7659bf426
RedOdd <redodd@yandex.ru>
2017-10-08 20:11:35 +0300

    cubeanimation

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject coub;
+    private bool canSwitchAnim = true;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+        if (canSwitchAnim) 
+        {
+            canSwitchAnim = false;
+            StartCoroutine(AnimDelay());
+
+        }
+    }
+
+    IEnumerator AnimDelay()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 5.0f));
+        GetComponent<Animator>().SetBool("isCanSwitchAnimToScale", true);
+        GetComponent<Animator>().SetBool("isCanSwitchAnimToMove", false);
+        yield return new WaitForSeconds(Random.Range(0.5f, 5.0f));
+        GetComponent<Animator>().SetBool("isCanSwitchAnimToMove", true);
+        GetComponent<Animator>().SetBool("isCanSwitchAnimToScale", false);
+        canSwitchAnim = true; 
+    }
+}
